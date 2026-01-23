f2adde848bf84dc232ca6e00245ba009d4dad6fe
mksdznk <maks.deznak@gmail.com>
2026-01-23 18:09:38 +0100

    Finished Lottery instance and Lottery Factory

--- contracts/Lottery.sol
+++ contracts/Lottery.sol
@@ -3,78 +3,115 @@ pragma solidity ^0.8.30;
 
 import {ERC20} from "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 import {ERC20Capped} from "@openzeppelin/contracts/token/ERC20/extensions/ERC20Capped.sol";
+import {ReentrancyGuard} from "@openzeppelin/contracts/security/ReentrancyGuard.sol";
+import {LotteryFactory} from "./LotteryFactory.sol";
+import {LotteryNFT} from "./LotteryNFT.sol";
  
 contract Lottery is ERC20Capped {
-    /*////////////// STATE VARIABLES /////////////*/
-    address private lotteryFactory;
+    /*///////////////////////////////////////////////
+                    STATE VARIABLES 
+    /*/////////////////////////////////////////////*/
     uint256 private lotteryId;
-    uint256 private maxTickets;
-    uint256 private maxTicketsPerPerson;
-    uint256 private fee;
-    Status private status;
+    uint256 private feePercentage; // in basis points
+    address private lotteryFactory;
 
-    mapping(address => uint256) private tickets;
-
-    enum Status { 
-        Open,
-        Pending,
-        Closed
-    }
-
-    /*////////////////// ERRORS //////////////////*/
+    /*//////////////////////////////////////////////
+                        ERRORS 
+    /*////////////////////////////////////////////*/
     error Lottery__NotOpen();
-    error Lottery_TicketsLimitReached();
+    error Lottery__NotPending();
+    error Lottery__TicketsLimitReached();
+    error Lottery__ZeroValue();
+    error Lottery__CallFailed();
 
-    /*////////////////// EVENTS //////////////////*/
+    /*//////////////////////////////////////////////
+                        EVENTS 
+    /*////////////////////////////////////////////*/
     event Lottery__TicketsPurchased(address buyer, uint256 amount);
+    event Lottery__TicketsTransfered(address sender, address buyer, uint256 amount);
+    event Lottery__FeesWithdrawn(address recipient, uint256 amount);
     event Lottery__LotteryClosed(address winner, uint256 reward);
 
-    /*//////////////// MODIFIERS ////////////////*/
+    /*//////////////////////////////////////////////
+                        MODIFIERS 
+    /*////////////////////////////////////////////*/
     modifier lotteryIsOpen() {
-        if (status != Status.Open) revert Lottery__NotOpen();
+        if (address(this).balance < cap()) revert Lottery__TicketsLimitReached();
         _;
     }    
 
+    modifier onlyFactory() {
+        require(msg.sender == lotteryFactory, Lottery__NotPending());
+        _;
+    }
+
+    modifier setPendingWinner() {
+        _;
+        if (totalSupply() == cap()) {
+            LotteryFactory(lotteryFactory).setLotteryPendingWinner(true);
+        }
+    }
+
     /*/////////////// CONSTRUCTOR ///////////////*/
-    constructor (uint256 _lotteryId, uint256 _cap, uint256 _fee) ERC20("Lottery Ticket", "LT") ERC20Capped(_cap) {
-        lotteryFactory = msg.sender;
+    constructor (uint256 _lotteryId, uint256 _cap, uint256 _feePercentage) 
+        ERC20("Lottery Ticket", "LT") 
+        ERC20Capped(_cap)
+    {
         lotteryId = _lotteryId;
-        fee = _fee;
+        feePercentage = _feePercentage;
+        lotteryFactory = msg.sender;
     }
      
-    /*//////////// EXTERNAL FUNCTIONS ////////////*/
-    function purchaseTickets() external returns (uint256 amount) {
-        //IMPLEMENT
+    /*//////////////////////////////////////////////
+                    EXTERNAL FUNCTIONS 
+    /*////////////////////////////////////////////*/
+    function purchaseTickets() external payable lotteryIsOpen setPendingWinner {
+        uint256 tickets = msg.value;
+        require(tickets > 0, Lottery__ZeroValue());
+
+        _mint(msg.sender, tickets);
+        emit Lottery__TicketsPurchased(msg.sender, tickets);
     }
 
-    function closeLottery() external {
-        //IMPLEMENT
+    function transferTickets(address to, uint256 amount) external {
+        _transfer(msg.sender, to, amount);  // ← this calls _update(from=msg.sender, to, amount) internally
+        emit Lottery__TicketsTransfered(msg.sender, to, amount);
     }
 
-    /*//////////// PRIVATE FUNCTIONS /////////////*/
-    
-    /*//////////// INTERNAL FUNCTIONS ////////////*/
+    function endLottery(address lotteryWinner) external onlyFactory {
+        uint256 fee = address(this).balance * (10000 / feePercentage);
 
-    /*////////////// VIEW FUNCTIONS //////////////*/
+        (bool success, ) = lotteryWinner.call{value: address(this).balance - fee}("");
+        require(success, Lottery__CallFailed());
+
+        (bool feeSuccess, ) = lotteryFactory.call{value: fee}("");
+        require(feeSuccess, Lottery__CallFailed());
+
+        ///////////////////////
+            /* NFT LOGIC */
+        ///////////////////////
+
+        LotteryFactory(lotteryFactory).setActiveLottery(address(0));
+        LotteryFactory(lotteryFactory).setLotteryPendingWinner(false);
+        emit Lottery__LotteryClosed(lotteryWinner, address(this).balance - fee);
+    }
+
+    /*//////////////////////////////////////////////
+                      VIEW FUNCTIONS 
+    /*////////////////////////////////////////////*/
     function getLotteryId() public view returns (uint256) {
         return lotteryId;
     }
 
-    function getFee() public view returns (uint256) {
-        return fee;
-    }
-
-    function getStatus() public view returns (Status) {
-        return status;
+    function getFeePercentage() public view returns (uint256) {
+        return feePercentage;
     }
 
     function getMaxTickets() public view returns (uint256) {
-        return maxTickets;
+        return cap();
     }
 
-    function getMaxTicketsPerPerson() public view returns (uint256) {
-        return maxTicketsPerPerson;
+    function getLotteryFactory() public view returns (address) {
+        return lotteryFactory;
     }
-
-    function get
 }
--- contracts/LotteryFactory.sol
+++ contracts/LotteryFactory.sol
@@ -5,31 +5,71 @@ pragma solidity ^0.8.30;
 import {Lottery} from "./Lottery.sol";
 import {Ownable} from "@openzeppelin/contracts/access/Ownable.sol";
 import {Ownable2Step} from "@openzeppelin/contracts/access/Ownable2Step.sol";
+import {Create2} from "@openzeppelin/contracts/utils/Create2.sol";
 
 contract LotteryFactory is Ownable2Step {
-    /*///////////// STATE VARIABLES /////////////*/
-    uint256 private lotteryCount;
 
-    /*////////////////// ERRORS //////////////////*/
+/*//////////////////////////////////////////////
+                STATE VARIABLES 
+/*////////////////////////////////////////////*/
+    uint256 private lotteryCount;
+    address private activeLottery;
+    bool private lotteryPendingWinner;
+
+/*//////////////////////////////////////////////
+                    ERRORS 
+/*////////////////////////////////////////////*/
     error LotteryFactory__ZeroAmount();
     error LotteryFactory__InsuffifientAccumulatedFees();
     error LotteryFactory__InvalidRecipient();
     error LotteryFactory__CallFailed();
+    error LotteryFactory__ActiveLotteryExists();
+    error LotteryFactory__NotLottery();
 
-
-    /*////////////////// EVENTS //////////////////*/
+/*//////////////////////////////////////////////
+                    EVENTS 
+/*////////////////////////////////////////////*/
     event LotteryFactory__LotteryCreated(address lottery);
     event LotteryFactory__FeesWithdrawn(address recipient, uint256 amount);
 
-    /*//////////////// MODIFIERS ////////////////*/
+/*//////////////////////////////////////////////
+                  MODIFIERS 
+/*////////////////////////////////////////////*/
+    modifier onlyLottery() {
+        // require(address(msg.sender).data == lotterydata, LotteryFactory__NotLottery());
+        _;
+    }
 
-    /*/////////////// CONSTRUCTOR ///////////////*/
-    constructor () Ownable(msg.sender) {}
+    modifier onlyOracle() {
+        // require(address(msg.sender).data == oracledata, LotteryFactory__NotOracle());
+        _;
+    }
 
-    /*///////////// PUBLIC FUNCTIONS /////////////*/
+/*/////////////// CONSTRUCTOR ///////////////*/
+    constructor () {}
 
-    /*//////////// EXTERNAL FUNCTIONS ////////////*/
-    function createLottery() external returns (address lottery) {
+/*//////////////////////////////////////////////
+                EXTERNAL FUNCTIONS
+/*////////////////////////////////////////////*/
+    function createLottery(uint256 cap, uint256 fee) external onlyOwner returns (address lottery) {
+        //IMPLEMENT
+        require(!lotteryPendingWinner, LotteryFactory__ActiveLotteryExists());
+        
+        lottery = address(new Lottery{salt: bytes32(lotteryCount)}(lotteryCount, cap, fee));
+        emit LotteryFactory__LotteryCreated(lottery);
+        lotteryCount++;
+        activeLottery = lottery;
+    }
+
+    function endLottery(address lottery, address lotteryWinner) external onlyOwner {
+        require(lottery == activeLottery, LotteryFactory__NotLottery());
+        require(lotteryWinner != address(0), LotteryFactory__InvalidRecipient());
+
+        ///////// Implement lottery ending
+        Lottery(lottery).endLottery(lotteryWinner);
+        /////////
+
+        activeLottery = address(0);
         //IMPLEMENT
     }
 
@@ -43,12 +83,26 @@ contract LotteryFactory is Ownable2Step {
         emit LotteryFactory__FeesWithdrawn(recipient, amount);
     }
 
-    /*//////////// PRIVATE FUNCTIONS ////////////*/
-    
-    /*//////////// INTERNAL FUNCTIONS ////////////*/
+    function setActiveLottery(address lottery) external onlyLottery {
+        activeLottery = lottery;
+    }
 
-    /*////////////// VIEW FUNCTIONS //////////////*/
+    function setLotteryPendingWinner(bool pending) external onlyLottery {
+        lotteryPendingWinner = pending;
+    }
+
+/*//////////////////////////////////////////////
+                VIEW FUNCTIONS 
+/*////////////////////////////////////////////*/
     function getLotteryCount() public view returns (uint256) {
         return lotteryCount;
+    }
+
+    function getActiveLottery() public view returns (address) {
+        return activeLottery;
+    }
+
+    function getLotteryPendingWinner() public view returns (bool) {
+        return lotteryPendingWinner;
     }
 }

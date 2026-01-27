685342a72ab6025a59dff44bcc4a44cff0cb556e
mksdznk <maks.deznak@gmail.com>
2026-01-27 22:25:50 +0100

    Added initial version of offchain ticket holders calculation

---
+++ script/LotteryWinner.ts
@@ -0,0 +1,90 @@
+//CHAINLINK SCRIPT FOR LOTTERY WINNER
+try {
+  const lotteryAddress = args[0];
+  const ticketPriceInWei = args[1];
+  let chain;
+  if (args[2] == 1) {
+      chain = 'eth';
+  }
+  else if (args[2] == 11155111) {
+      chain = 'sepolia';
+  }
+  console.log(chain);
+  let response = await Functions.makeHttpRequest({
+    method: 'GET',
+    url: `https://deep-index.moralis.io/api/v2.2/erc20/${lotteryAddress}/owners`,
+    params: {
+      chain: chain,
+      limit: '100',
+      order: 'DESC'
+    },
+    headers: {
+      accept: 'application/json',
+      'X-API-Key': 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJub25jZSI6IjM3NWJhN2U0LWI5OGItNDMyMS04ZDZlLWU2ZjFlZjBlOGJkOSIsIm9yZ0lkIjoiNDk0MTg2IiwidXNlcklkIjoiNTA4NTM1IiwidHlwZUlkIjoiYjEzNGI4YmMtMjA3MS00ODk4LWE3OTItZDU0MjlmNGE2ZDdjIiwidHlwZSI6IlBST0pFQ1QiLCJpYXQiOjE3NjkxNzkzNzEsImV4cCI6NDkyNDkzOTM3MX0.IFUfq-1G7tyUOFFfsM4lsWEQJzY9qiZHtP_1KSuv5Hk'
+    }
+  });
+
+    let lotteryEntries = response.data.result;
+    let lotteryAddresses = [];
+    let entriesForAddress = 0;
+    let cursor = response.data.cursor;
+    console.log(cursor);
+
+    for (let i = 0; i < lotteryEntries.length; i++) {
+        entriesForAddress = Math.floor(lotteryEntries[i].balance / ticketPriceInWei);
+        for (let j = 0; j < entriesForAddress; j++) {
+          lotteryAddresses.push(lotteryEntries[i]);
+        }
+    }
+
+    //cusor points to next page if too many data entries for one API call
+    while (cursor != null) {
+      response = await Functions.makeHttpRequest({
+        method: 'GET',
+        url: `https://deep-index.moralis.io/api/v2.2/erc20/${lotteryAddress}/owners`,
+        params: {
+          chain: chain,
+          limit: '100',
+          order: 'DESC',
+          cursor: cursor,
+        },
+        headers: {
+          accept: 'application/json',
+          'X-API-Key': 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJub25jZSI6IjM3NWJhN2U0LWI5OGItNDMyMS04ZDZlLWU2ZjFlZjBlOGJkOSIsIm9yZ0lkIjoiNDk0MTg2IiwidXNlcklkIjoiNTA4NTM1IiwidHlwZUlkIjoiYjEzNGI4YmMtMjA3MS00ODk4LWE3OTItZDU0MjlmNGE2ZDdjIiwidHlwZSI6IlBST0pFQ1QiLCJpYXQiOjE3NjkxNzkzNzEsImV4cCI6NDkyNDkzOTM3MX0.IFUfq-1G7tyUOFFfsM4lsWEQJzY9qiZHtP_1KSuv5Hk'
+        }
+      });
+
+      lotteryEntries = response.data.result;
+      entriesForAddress = 0;
+      cursor = response.data.cursor;
+
+      console.log("cursor")
+      console.log('Response:', response.data.page);
+
+      for (let i = 0; i < lotteryEntries.length; i++) {
+        entriesForAddress = Math.floor(lotteryEntries[i].balance / ticketPriceInWei);
+        for (let j = 0; j < entriesForAddress; j++) {
+          lotteryAddresses.push(lotteryEntries[i]);
+        }
+      }
+    }
+
+  if (response.error) {
+    console.log('Response:', response);
+    throw new Error(`API error: ${response.error}`);
+  }
+
+    // Return the data properly encoded
+    return Functions.encodeString("test");
+    // return Functions.encodeString(JSON.stringify({
+    //     success: true,
+    //     data: response.data
+    // }));
+
+} catch (error) {
+  console.error('Error:', error);
+  return Functions.encodeString(JSON.stringify({
+    success: false,
+    error: error.message
+  }));
+}

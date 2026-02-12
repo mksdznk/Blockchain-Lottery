8cbc5b38a2f81ac9f0d92a1030739d89283a2bda
mksdznk <maks.deznak@gmail.com>
2026-02-12 10:15:34 +0100

    Fixed contract configs used for wagmi hooks

--- front-end/src/contracts/lotteryContractConfig.ts
+++ front-end/src/contracts/lotteryContractConfig.ts
@@ -2,6 +2,6 @@
 import { abi } from "../abis/Lottery.json";
  
 export const lotteryContractConfig = {
-    address: '' as const, // testToken contract address
+    // address: '' as const, // testToken contract address
     abi: abi,
   } as const;
--- front-end/src/contracts/lotteryFactoryContractConfig.ts
+++ front-end/src/contracts/lotteryFactoryContractConfig.ts
@@ -1,5 +1,5 @@
 // contracts.ts
-import { abi } from "../abis/LotteryFactory.json";
+import { abi } from "../abis/LotteryFunctions.json";
  
 export const lotteryFactoryContractConfig = {
     address: '0x9594e708d6C15920540FF0c02729A4bDc7B93eca' as const,

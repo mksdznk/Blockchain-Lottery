d46f8b1aa39325212803e0604860ce30da9972a1
mksdznk <maks.deznak@gmail.com>
2026-02-14 12:29:49 +0100

    Added Lottery NFT contract

--- contracts/LotteryNFT.sol
+++ contracts/LotteryNFT.sol
@@ -2,26 +2,29 @@
 pragma solidity ^0.8.30;
 
 import {ERC721} from "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+import {Ownable} from "@openzeppelin/contracts/access/Ownable.sol";
 
-contract LotteryNFT is ERC721 {
+contract LotteryNFT is ERC721, Ownable {
     /*///////////// STATE VARIABLES /////////////*/
+    uint256 private constant MAX_SUPPLY = 6;
+    uint256 private counter = 0;
+    
 
     /*////////////////// ERRORS //////////////////*/
-
-    /*////////////////// EVENTS //////////////////*/
-
-    /*//////////////// MODIFIERS ////////////////*/
+    error LotteryNFT__MaxSupplyReached();
 
     /*/////////////// CONSTRUCTOR ///////////////*/
     constructor() ERC721("Fifu Fifu", "FIFU2") {}
 
-    /*///////////// PUBLIC FUNCTIONS /////////////*/
-
     /*//////////// EXTERNAL FUNCTIONS ////////////*/
+    function mint(address to) external onlyOwner {
+        require(counter < MAX_SUPPLY, LotteryNFT__MaxSupplyReached());
 
-    /*//////////// PRIVATE FUNCTIONS ////////////*/
-
+        _safeMint(to, counter);
+        counter += 1;
+    }
     /*//////////// INTERNAL FUNCTIONS ////////////*/
-
-    /*////////////// VIEW FUNCTIONS //////////////*/
+    function _baseURI() internal view override returns (string memory) {
+        return "ipfs://bafybeifatqoahypgwbg2oyv5gj7nwd6b7weqc7b3wqtpajscg5l7inpium/";
+    }
 }

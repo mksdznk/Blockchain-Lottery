2c23a4c4c38dbc6648d684b184ec6690a156e258
mksdznk <maks.deznak@gmail.com>
2026-02-24 11:12:48 +0100

    Added .env.sample for setup

--- front-end/src/config/config.ts
+++ front-end/src/config/config.ts
@@ -1,13 +1,12 @@
 // config/config.ts
-import { getDefaultConfig } from '@rainbow-me/rainbowkit'
 import { createConfig, http } from 'wagmi'
 import { mainnet,sepolia } from 'wagmi/chains'
 
 export const config = createConfig({
   chains: [sepolia, mainnet],
   transports: {
-    [sepolia.id]: http("https://eth-sepolia.g.alchemy.com/v2/uuhYlUhc8b-FX4-r1W9vf"),
-    [mainnet.id]: http("https://eth-mainnet.g.alchemy.com/v2/uuhYlUhc8b-FX4-r1W9vf"),
+    [sepolia.id]: http(process.env.SEPOLIA_RPC_URL),
+    [mainnet.id]: http(process.env.SEPOLIA_RPC_URL),
   },
   ssr: true,
 })
